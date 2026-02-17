2a7e104951580254e0b99b6ff967c98bb186fd6d
josueSR2 <josuesierraxd66@gmail.com>
2026-02-17 16:20:51 -0400

    Commit realizado con éxito.

--- Middleware_Core/obj/Debug/net8.0/Middleware_Core.AssemblyInfo.cs
+++ Middleware_Core/obj/Debug/net8.0/Middleware_Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Middleware_Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+432697386b0de8772b37997248e877403abab139")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b0eae969bebea424e0ee7e4e533e6a83b66f141")]
 [assembly: System.Reflection.AssemblyProductAttribute("Middleware_Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Middleware_Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
