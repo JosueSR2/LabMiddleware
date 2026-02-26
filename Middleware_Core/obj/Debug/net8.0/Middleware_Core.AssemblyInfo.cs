00d3bf7c6d7030e327f48b9d3ee910b996d05a05
josueSR2 <josuesierraxd66@gmail.com>
2026-02-26 09:29:43 -0400

    Commit de cambios en la rama main

--- Middleware_Core/obj/Debug/net8.0/Middleware_Core.AssemblyInfo.cs
+++ Middleware_Core/obj/Debug/net8.0/Middleware_Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Middleware_Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb7add9257923337f8aec8ed625fe1fb09adc33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a1677919ac5228cf33c00bdf7644f052bf6ec1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Middleware_Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Middleware_Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
